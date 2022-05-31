ab7d2b16d36a21344891bf6fe2df2039cf6131aa
codesktop <codesktop@foxmail.com>
2022-05-31 23:51:25 +0800

    tossode <82136882+tossode@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const { promises: dns } = require('dns')
 const isEmpty = require('lodash/isEmpty')
-const isURL = require('validator/lib/isURL')
 
 async function dnsLookup(domain) {
   try {
@@ -14,11 +13,6 @@ module.exports = async (request, response) => {
   const address = request.headers['x-real-ip']
   const domain = (request.query.domain ||
                   request.query.d)
-
-  if (isURL(domain)) {
-    const { host } = new URL(domain)
-    return response.redirect(`/?d=${host}`)
-  }
 
   return isEmpty(domain)
     ? response.json({ address })
