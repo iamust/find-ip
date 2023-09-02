2344d2972e06e3b2bacb3740f3b8b021e24a6ce2
razeos <65535758+razeos@users.noreply.github.com>
2023-09-02 15:36:56 +0800

    neweos created a commit :sparkles:

--- api/_deno.ts
+++ api/_deno.ts
@@ -1,4 +1,6 @@
 
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import dns from 'node:dns/promises';
 
+console.log(dns)
 serve((req: Request) => new Response("Hello Deno!"));
