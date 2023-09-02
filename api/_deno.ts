b52d7bec2a06465eb8e6d1d5bc79e9765aadd260
razeos <65535758+razeos@users.noreply.github.com>
2023-09-02 15:58:08 +0800

    neweos created a commit :sparkles:

--- api/_deno.ts
+++ api/_deno.ts
@@ -1,9 +1,9 @@
 
-import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+// import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 import dns from 'node:dns/promises';
 
 // console.log(dns)
-serve(async (req: Request) => {
+Deno.serve(async (req: Request) => {
   console.log(req.headers);
   const data = await dns.lookup('cloudflare.com', { all: true });
   return new Response(JSON.stringify(data));
