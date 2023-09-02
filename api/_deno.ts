29e548b2f41edfa1b7a664c73cec71dc240fe2ab
razeos <65535758+razeos@users.noreply.github.com>
2023-09-02 15:48:56 +0800

    neweos created a commit :sparkles:

--- api/_deno.ts
+++ api/_deno.ts
@@ -2,5 +2,9 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 import dns from 'node:dns/promises';
 
-console.log(dns)
-serve((req: Request) => new Response("Hello Deno!"));
+// console.log(dns)
+serve(async (req: Request) => {
+  console.log(req.headers.get('x-real-ip'));
+  const data = await dns.lookup('kakao.com', { all: true });
+  return new Response(JSON.stringify(data));
+});
