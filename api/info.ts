60bd994ac21fc7b4c52c9fd4fe7b553f7a3b7608
razeos <65535758+razeos@users.noreply.github.com>
2023-09-04 01:25:44 +0800

    neweos created a commit :sparkles:

--- api/info.ts
+++ api/info.ts
@@ -2,9 +2,13 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import amap from '../lib/helpers/amap'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const { data } = await amap.get('/ip', {
-    params: { ip: '119.120.221.151' }
-  })
+  try {
+    const { data } = await amap.get('/ip', {
+      params: { ip: '119.120.221.151' }
+    })
 
-  return response.json(data)
+    return response.json(data)
+  } catch ({ message }) {
+    return response.json({ message })
+  }
 }
