1d11cf2467c4569a1cfa910d0f5f741e88f7fb97
razeos <65535758+razeos@users.noreply.github.com>
2023-09-05 23:37:42 +0800

    neweos created a commit :sparkles:

--- api/info.ts
+++ api/info.ts
@@ -1,16 +1,29 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-import amap from '../lib/helpers/amap'
+import { type Context, Hono } from 'hono'
+import { handle } from 'hono/vercel'
 
-export default async (request: VercelRequest, response: VercelResponse) => {
-  const ip = request.query.ip || request.headers['x-real-ip']
+const app = new Hono()
 
-  try {
-    const { data } = await amap.get('/ip', {
-      params: { ip }
-    })
+app.get('/info', async (c: Context) => {
+  return c.text(c.req.header['x-real-ip'])
+})
 
-    return response.json(data)
-  } catch ({ message }) {
-    return response.json({ message })
-  }
-}
+export default handle(app)
+
+// import type { VercelRequest, VercelResponse } from '@vercel/node'
+// import axios from 'axios'
+
+// export default async (request: VercelRequest, response: VercelResponse) => {
+//   const ip = request.query.ip || request.headers['x-real-ip']
+
+//   try {
+//     const { data } = await axios.get('https://restapi.amap.com/v3/ip', {
+//       params: {
+//         key: process.env.AMAP_API_KEY,
+//       }
+//     })
+
+//     return response.json(data)
+//   } catch ({ message }) {
+//     return response.json({ message })
+//   }
+// }
--- lib/helpers/amap.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: 'https://restapi.amap.com/v3',
-  params: {
-    key: process.env.AMAP_API_KEY
-  }
-})
