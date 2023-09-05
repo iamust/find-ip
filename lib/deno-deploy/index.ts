9061acdc192c8eb154cb79bf600b3d88bb5b570a
razeos <65535758+razeos@users.noreply.github.com>
2023-09-05 21:48:49 +0800

    neweos created a commit :sparkles:

--- lib/deno-deploy/index.ts
+++
@@ -1,6 +0,0 @@
-import { type ConnInfo, serve } from 'https://deno.land/std@0.195.0/http/server.ts'
-
-serve((request: Request, { remoteAddr }: ConnInfo) => {
-  const { hostname } = remoteAddr as Deno.NetAddr
-  return new Response(`${hostname}\n`)
-})
