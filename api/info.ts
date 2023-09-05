f6acffedc24951438eca6965721cc002d296c9fe
razeos <65535758+razeos@users.noreply.github.com>
2023-09-06 00:55:31 +0800

    neweos created a commit :sparkles:

--- api/info.ts
+++ api/info.ts
@@ -1,29 +1,17 @@
-import { handle } from '@hono/node-server/vercel'
-import { type Context, Hono } from 'hono'
+import type { VercelRequest, VercelResponse } from '@vercel/node'
 import axios from 'axios'
 
-const app = new Hono()
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const ip = request.query.ip || request.headers['x-real-ip']
+  const key = process.env.AMAP_API_KEY
 
-app.get('/info', async (c: Context) => {
-  // console.log()
-  return c.json(c.req.headers)
-})
+  try {
+    const { data } = await axios.get('https://restapi.amap.com/v3/ip', {
+      params: { key, ip }
+    })
 
-export default handle(app)
-
-// import type { VercelRequest, VercelResponse } from '@vercel/node'
-// export default async (request: VercelRequest, response: VercelResponse) => {
-//   const ip = request.query.ip || request.headers['x-real-ip']
-
-//   try {
-//     const { data } = await axios.get('https://restapi.amap.com/v3/ip', {
-//       params: {
-//         key: process.env.AMAP_API_KEY,
-//       }
-//     })
-
-//     return response.json(data)
-//   } catch ({ message }) {
-//     return response.json({ message })
-//   }
-// }
+    return response.json(data)
+  } catch ({ message }) {
+    return response.status(400).json({ message })
+  }
+}
