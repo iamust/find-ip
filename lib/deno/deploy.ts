f37339e4baec2618cc17f9502c46ddabc4ab2b22
razeos <65535758+razeos@users.noreply.github.com>
2023-09-05 22:12:15 +0800

    neweos created a commit :sparkles:

--- lib/deno/deploy.ts
+++ lib/deno/deploy.ts
@@ -3,23 +3,23 @@ import { type Context, Hono } from 'https://deno.land/x/hono@v3.5.8/mod.ts'
 import { cors } from 'https://deno.land/x/hono@v3.5.8/middleware.ts'
 
 const app = new Hono()
-app.use('/ns|ip/*', cors({
-  origin: '*',
-  allowMethods: ['GET', 'OPTIONS', 'POST'],
-}))
+
+app.use(
+  '/*',
+  cors({
+    origin: '*',
+    allowMethods: ['GET', 'OPTIONS', 'POST']
+  })
+)
 
 app.get('/', (c: Context) => {
   return c.text(`${c.env.hostname}\n`)
 })
 
-app.get('/ip/:domain', async (c: Context) => {
+app.get('/:domain', async (c: Context) => {
   const domain = c.req.param('domain')
-  const result = await Deno.resolveDns(domain, 'A')
-  return c.json({ result })
-})
 
-app.get('/ns/:domain', async (c: Context) => {
-  const domain = c.req.param('domain')
+  //   const result = await Deno.resolveDns(domain, 'A')
   const result = await Deno.resolveDns(domain, 'NS', {
     nameServer: { ipAddr: '8.8.8.8' }
   })
