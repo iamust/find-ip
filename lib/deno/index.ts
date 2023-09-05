38fdb6d9f6000a0ccdd86df03a647cfdc7374061
razeos <65535758+razeos@users.noreply.github.com>
2023-09-05 19:16:21 +0800

    neweos created a commit :sparkles:

--- lib/deno/index.ts
+++ lib/deno/index.ts
@@ -1,15 +1,22 @@
 import { type ConnInfo, serve } from 'https://deno.land/std@0.195.0/http/server.ts'
 import { type Context, Hono } from 'https://deno.land/x/hono@v3.5.8/mod.ts'
+import { cors } from 'https://deno.land/x/hono@v3.5.8/middleware/cors/index.ts'
 
 const app = new Hono()
+app.use('/ns|ip/*', cors({
+  origin: '*',
+  allowMethods: ['GET', 'OPTIONS', 'POST'],
+}))
 
 app.get('/', (c: Context) => {
   return c.text(`${c.env.hostname}\n`)
 })
 
-// app.get('/ip/:domain', (c: Context) => {
-//   return c.json({})
-// })
+app.get('/ip/:domain', async (c: Context) => {
+  const domain = c.req.param('domain')
+  const result = await Deno.resolveDns(domain, 'A')
+  return c.json({ result })
+})
 
 app.get('/ns/:domain', async (c: Context) => {
   const domain = c.req.param('domain')
