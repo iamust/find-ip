f045e92d0d6ae437c74d5d4ca99ac0a0902055f5
razeos <65535758+razeos@users.noreply.github.com>
2023-09-05 22:34:41 +0800

    neweos created a commit :sparkles:

--- lib/deno/deploy.ts
+++ lib/deno/deploy.ts
@@ -1,16 +1,7 @@
 import { type ConnInfo, serve } from 'https://deno.land/std@0.195.0/http/server.ts'
 import { type Context, Hono } from 'https://deno.land/x/hono@v3.5.8/mod.ts'
-import { cors } from 'https://deno.land/x/hono@v3.5.8/middleware.ts'
 
 const app = new Hono()
-
-app.use(
-  '*',
-  cors({
-    origin: '*',
-    allowMethods: ['GET', 'OPTIONS', 'POST']
-  })
-)
 
 app.get('/', (c: Context) => {
   return c.text(`${c.env.hostname}\n`)
@@ -19,12 +10,15 @@ app.get('/', (c: Context) => {
 app.get('/:domain', async (c: Context) => {
   const { domain } = c.req.param()
 
-  //   const result = await Deno.resolveDns(domain, 'A')
-  const result = await Deno.resolveDns(domain, 'NS', {
-    nameServer: { ipAddr: '8.8.8.8' }
-  })
+  try {
+    const result = await Deno.resolveDns(domain, 'NS', {
+      nameServer: { ipAddr: '1.1.1.1' }
+    })
 
-  return c.json({ result })
+    return c.json(result)
+  } catch ({ message }) {
+    return c.json({ message }, 400)
+  }
 })
 
 serve((request: Request, { remoteAddr }: ConnInfo) => {
