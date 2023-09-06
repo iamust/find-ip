db4f255b25bcf52c0018de9dbfff49f47ef6bc94
razeos <65535758+razeos@users.noreply.github.com>
2023-09-06 13:09:14 +0800

    neweos created a commit :sparkles:

--- api/index.ts
+++ api/index.ts
@@ -3,19 +3,25 @@ import isEmpty from 'lodash/isEmpty'
 import dns from 'dns/promises'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const address = request.headers['x-real-ip']
+  const ip = request.headers['x-real-ip']
   const domain =
     request.method === 'POST'
       ? request.body.domain
       : request.query.domain || request.query.d
 
   if (isEmpty(domain)) {
-    return response.json({ address })
+    return response.json({ ip })
   }
 
   try {
     const result = await dns.lookup(domain, { all: true })
-    return response.json(result)
+    return response.json(
+      result.map(({ address, family }) => ({
+        type: { 4: 'A', 6: 'AAAA' }[family],
+        value: address,
+        family
+      }))
+    )
   } catch ({ message }) {
     return response.status(400).json({ message })
   }
